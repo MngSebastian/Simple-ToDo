98e94111c48eeeb3beff78643ddba37a8533584c
Seb <m.sebi97@yahoo.com>
2023-01-05 10:59:13 +0100

    Main Component Done.

---
+++ src/components/AddTodo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AddTodo() {
+  return (
+    <div className='addTodo'>
+        <input/>
+        <button className='add'>Add</button>
+    </div>
+  )
+}
+
+export default AddTodo
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddTodo from './AddTodo'
+function Main() {
+  return (
+    <div className='main'>
+      <h2>Todo-List</h2>
+      <AddTodo />
+    </div>
+  )
+}
+
+export default Main
